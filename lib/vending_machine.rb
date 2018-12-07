2329d22248e1bb7900c362535e7779a5d72a8026
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-07 12:06:38 -0700

    Adds notes for future work.

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -3,13 +3,22 @@ require 'json'
 require 'pry'
 
 class VendingMachine
+  #pass in objects into the vending machine
+  #machine = Machine.new
+  #machine.slots
+  #machine.add_slot
+  #slot = Slot.new
+  # items = data[:contents].map
+  #   Item.new(c)
+  # end
+  #slot.add_items(items)
   attr_reader :vending_data
 
   def initialize(vending_data)
     @vending_data = Reader.new.read_data
   end
 
-  def contents
+  def slots
     @vending_data["contents"]
   end
 
