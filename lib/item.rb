4f11aa3c0bab87be2d29c2e79db46d578c1faf0a
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-08 19:16:24 -0700

    Adds attr reader.

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,5 @@
 class Item
+  attr_reader :name
   def initialize(item_data)
     @name = item_data["type"]
   end
--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -4,7 +4,7 @@ require 'json'
 require 'pry'
 
 class VendingMachine
-
+  attr_reader :slots
   def initialize(vending_data)
     @vending_data = Reader.new.read_data
     @slots = @vending_data["contents"].first.map do |slot_name, slot_data|
