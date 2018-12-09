16792f9943e9782ce467f59450511d312350933e
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-12-08 19:14:12 -0700

    Changes symbol to string.

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -4,11 +4,11 @@ require 'json'
 require 'pry'
 
 class VendingMachine
-  attr_reader :vending_data
+  # attr_reader :vending_data
 
   def initialize(vending_data)
     @vending_data = Reader.new.read_data
-    @slots = @vending_data[:contents].first.first.map do |slot_name, slot_data|
+    @slots = @vending_data["contents"].first.map do |slot_name, slot_data|
       Slot.new(slot_name, slot_data)
     end
   end
